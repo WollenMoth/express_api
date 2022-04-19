ce87ad8ece3fbf84b5ea34e1ab372919d125eb98
André Díaz <andre290800@gmail.com>
2022-04-19 18:54:11 -0500

    get explorer

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ app.get("/v1/explorers", (req, res) => {
   res.status(200).json(explorers);
 });
 
+app.get("/v1/explorers/:id", (req, res) => {
+  console.log(`API Explorers GET request ${new Date()}`);
+  console.log(`Getting explorer with id: ${req.params.id}`);
+
+  const explorer = { id: 1, name: "André" };
+
+  res.status(200).json(explorer);
+});
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
