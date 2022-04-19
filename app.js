61189e48830f1b6544bdce10e1d7fae38415c55c
André Díaz <andre290800@gmail.com>
2022-04-19 18:52:31 -0500

    get explorers list

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ app.use(express.json());
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000;
 
+// HTTP METHODS
+app.get("/v1/explorers", (req, res) => {
+  console.log(`API Explorers GET ALL request ${new Date()}`);
+
+  const explorer1 = { id: 1, name: "André1" };
+  const explorer2 = { id: 2, name: "André2" };
+  const explorer3 = { id: 3, name: "André3" };
+  const explorer4 = { id: 4, name: "André4" };
+  const explorers = [explorer1, explorer2, explorer3, explorer4];
+
+  res.status(200).json(explorers);
+});
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
